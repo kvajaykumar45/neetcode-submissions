4446dc2a1e90ee1143012173bc97c84b52ad363a
Ajay Kumar <133203707+kvajaykumar45@users.noreply.github.com>
2026-06-17 13:16:16 +0530

    Add: non-cyclical-number - submission-1

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def square(self, n):
+        s = 0
+        while n!=0:
+            r = n%10
+            s += r*r
+            n = n//10
+        return s
+    
+    def isHappy(self, n: int) -> bool:
+        h = set()
+        h.add(n)
+        while True:
+            n = self.square(n)
+            if n == 1:
+                return True
+            if n not in h:
+                h.add(n)
+            else:
+                return False
+            
+ 
+
+        
+
+
+        
