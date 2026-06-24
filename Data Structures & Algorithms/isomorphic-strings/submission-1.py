fbd713b680e79d678e5654964ae5df4acb7fe277
Ajay Kumar <133203707+kvajaykumar45@users.noreply.github.com>
2026-06-24 09:21:26 +0530

    Add: isomorphic-strings - submission-1

---
+++ Data Structures & Algorithms/isomorphic-strings/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        m1, m2 = {}, {}
+        for i in range(len(s)):
+            ch1, ch2 = s[i], t[i]
+            if (ch1 in m1 and m1[ch1] != ch2) or (ch2 in m2 and m2[ch2] != ch1):
+                return False
+            m1[ch1] = ch2
+            m2[ch2] = ch1
+        return True
+
+
+        
