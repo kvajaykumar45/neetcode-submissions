1a7c7091f2e95971cb3e5315e6cf57d365c4453a
Ajay Kumar <133203707+kvajaykumar45@users.noreply.github.com>
2026-07-01 10:22:19 +0530

    Add: number-of-good-pairs - submission-2

---
+++ Data Structures & Algorithms/number-of-good-pairs/submission-2.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        d = {}
+        for each in nums:
+            d[each] = d.get(each,0)+1
+        total = 0
+        for each in d.values():
+            total += (each*(each-1))//2
+        return total
