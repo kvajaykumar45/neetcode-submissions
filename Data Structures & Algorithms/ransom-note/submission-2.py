371077b47700d02f2e7df275f1b9835bb0cd7cf1
Ajay Kumar <133203707+kvajaykumar45@users.noreply.github.com>
2026-07-09 19:36:53 +0530

    Add: ransom-note - submission-2

---
+++ Data Structures & Algorithms/ransom-note/submission-2.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        c1 = Counter(ransomNote)
+        c2 = Counter(magazine)
+        for k,v in c1.items():
+            if k not in c2:
+                return False
+            elif c1[k] > c2[k]:
+                return False
+        return True
